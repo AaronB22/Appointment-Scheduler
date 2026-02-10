811953f4343c356d519482eb1b31bb5d63d97e2f
AaronB22 <aaronthesnowboarder@live.com>
2026-02-10 14:31:24 -0800

    added submit route

--- app.js
+++ app.js
@@ -3,13 +3,25 @@ import express from 'express';
 const app = express();
 
 const PORT = 3000;
+app.use(express.urlencoded({ extended: true }));
+
+const appointments=[]
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    // res.sendFile(`${import.meta.dirname}/views/home.html`);
-    res.send("Sup")
+    res.sendFile(`${import.meta.dirname}/views/index.html`);
 });
+app.post('/submit', (req, res)=>{
+    appointments.push({
+        fName: req.body.fName,
+        lName: req.body.lname,
+        date: req.body.date,
+        time: req.body.time,
+        timeStamp: new Date()
+    })
+
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`)
